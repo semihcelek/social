c5288b5e9f1238527770fb95bbf8f0990f752f3c
Semih Celek <cybermoon1999@gmail.com>
2021-04-03 01:03:13 +0300

    Add Post and Meetup Models. Create association User hasMany Post.

--- server/src/sequelize.js
+++ server/src/sequelize.js
@@ -2,6 +2,8 @@ const { Sequelize, DataTypes } = require("sequelize");
 const pg = require("pg");
 
 const UserModel = require("./models/user-model");
+const PostModel = require("./models/post-model");
+const MeetupModel = require("./models/meetup-model");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -16,7 +18,17 @@ const sequelize = new Sequelize(
 (async () => await sequelize.sync({ alter: true }))();
 
 const User = UserModel(sequelize, DataTypes);
+const Post = PostModel(sequelize, DataTypes);
+const Meetup = MeetupModel(sequelize, DataTypes);
+
+User.hasMany(Post, { foreignKey: "posts" });
+Post.belongsTo(User, { foreignKey: "author" });
+
+// User.belongsToMany(Meetup)
+// In order to implement many-to-many relationship, it is neccessary to create a middleground table.
 
 module.exports = {
   User,
+  Post,
+  Meetup,
 };
