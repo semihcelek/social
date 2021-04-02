278b533b0196e34a99c3b622dab9e0e2d5955aab
Semih Celek <cybermoon1999@gmail.com>
2021-04-03 00:03:56 +0300

    Add User Model

---
+++ server/src/models/user-model.js
@@ -0,0 +1,46 @@
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("User", {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    username: {
+      type: DataTypes.STRING(80),
+      allowNull: false,
+      unique: true,
+    },
+    firstName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING(50),
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: 6,
+      },
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      defaultValue: "avatar-link-in-here",
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+  });
+};
