036b930f459cf16e70bba29b79ac461901fe6e61
Semih Celek <cybermoon1999@gmail.com>
2021-04-03 15:33:35 +0300

    Add new routes to App.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const helmet = require("helmet");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const userRouter = require("./routes/user-route");
+const registerRouter = require("./routes/register-route");
+const loginRouter = require("./routes/login-route");
 
 const app = express();
+
+app.use(helmet());
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,6 +20,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/user", userRouter);
+app.use("/register", registerRouter);
+app.use("/login", loginRouter);
 
 module.exports = app;
