a3bd2626b170ce5f1d638b4069ccce00a43c658a
Semih Celek <cybermoon1999@gmail.com>
2021-04-03 15:18:15 +0300

    Update User model to prevent querying password all the time
    Default behavior of queries is get all the entries on a entity, including passwords and other data that we dont want to get on every query.
    So I changed default scope to not to get password. When we need password, we need to select password enty additionaly.

--- server/src/models/user-model.js
+++ server/src/models/user-model.js
@@ -1,46 +1,54 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("User", {
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      primaryKey: true,
-      defaultValue: Sequelize.UUIDV4,
-    },
-    username: {
-      type: DataTypes.STRING(80),
-      allowNull: false,
-      unique: true,
-    },
-    firstName: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING(50),
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
+  return sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      username: {
+        type: DataTypes.STRING(80),
+        allowNull: false,
+        unique: true,
+      },
+      firstName: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING(50),
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          min: 6,
+        },
+      },
+      avatar: {
+        type: DataTypes.STRING,
+        defaultValue: "avatar-link-in-here",
+      },
+      description: {
+        type: DataTypes.STRING,
       },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        min: 6,
+    {
+      defaultScope: {
+        attributes: { exclude: ["password"] },
       },
-    },
-    avatar: {
-      type: DataTypes.STRING,
-      defaultValue: "avatar-link-in-here",
-    },
-    description: {
-      type: DataTypes.STRING,
-    },
-  });
+    }
+  );
 };
