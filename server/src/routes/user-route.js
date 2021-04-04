d4aaf8136a6b4b671c65c82852a00a0a8f0e8a5a
Semih Celek <cybermoon1999@gmail.com>
2021-04-04 19:11:15 +0300

    Update User route, add new routes
    User should populate it's posts but it doesn't, look sequelize docs

--- server/src/routes/user-route.js
+++ server/src/routes/user-route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const auth = require("../middlewares/auth");
-const { User } = require("../sequelize");
+const { User, Post } = require("../sequelize");
 const router = express.Router();
 
 router.get("/all/json", async (req, res, next) => {
@@ -13,13 +13,63 @@ router.get("/all/json", async (req, res, next) => {
 
 router.get("/:id/json", auth, async (req, res, next) => {
   const person = await User.findOne({
-    // include: posts,
+    include: Post,
     where: {
       id: req.params.id,
     },
   });
   console.log(person);
   person ? res.send(person) : res.status(404).end();
+});
+router.get("/:id/posts/json", auth, async (req, res, next) => {
+  const posts = await Post.findAll({
+    where: {
+      author: req.params.id,
+    },
+  });
+
+  posts ? res.send(posts) : res.status(404).end();
+});
+
+router.put("/:id", auth, async (req, res) => {
+  const person = req.user;
+  try {
+    if (person) {
+      const updateUser = await User.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.send(updateUser);
+    } else {
+      res.send("user is not found");
+      res.status(404).end();
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const person = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (person) {
+      await User.destroy({
+        where: {
+          id: req.params.id,
+        },
+      }).then(res.send("account is succesfully deleted"));
+    } else {
+      res.send("User is not found");
+      res.status(404).end();
+    }
+  } catch (err) {
+    console.err(err);
+  }
 });
 
 module.exports = router;
