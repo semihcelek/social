b9123c455855cff80ac94f45906adec16266fe6f
Semih Celek <cybermoon1999@gmail.com>
2021-04-05 20:25:19 +0300

    Add User Service
    getLocalUser and saveLocalUser saves user object to localstore when login happens.

---
+++ client/src/services/user-service.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const loginUser = async (values) => {
+  const response = await axios.post(
+    `${process.env.REACT_APP_API_URL}/login`,
+    values
+  );
+  console.log(response.data);
+  if (response.data) {
+    return response.data;
+  } else {
+    return null;
+  }
+};
+export const registerUser = async (values) => {
+  const response = await axios.post(
+    `${process.env.REACT_APP_API_URL}/register`,
+    values
+  );
+  return response.data;
+};
+
+export const getLocalUser = () => {
+  const getLocalSavedUser = window.localStorage.getItem("social-app-user");
+  if (getLocalSavedUser) {
+    const user = JSON.parse(getLocalSavedUser);
+    console.log(user);
+    return user;
+  } else {
+    return {};
+  }
+};
+
+export const saveLocalUser = (userObj) => {
+  const saveUser = window.localStorage.setItem(
+    "social-app-user",
+    JSON.stringify(userObj)
+  );
+  console.log(saveUser);
+};
