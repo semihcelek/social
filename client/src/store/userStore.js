31677399ac406ba74d198553bd57aea52ba28547
Semih Celek <cybermoon1999@gmail.com>
2021-04-05 20:21:22 +0300

    Initialize Zustand state-management lib for User state
    Zustand is a very simple state-management library based on redux and has very little boilerplate.

---
+++ client/src/store/userStore.js
@@ -0,0 +1,13 @@
+import create from "zustand";
+import { devtools } from "zustand/middleware";
+import { getLocalUser } from "../services/user-service";
+
+const useUserStore = create(
+  devtools((set) => ({
+    // calling a function to initialize the state feels wrong, search for it.
+    user: getLocalUser(),
+    setUser: (user) => set((state) => ({ ...state, user: user })),
+  }))
+);
+
+export { useUserStore };
