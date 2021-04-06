dc012a39e0f5b5bfc75c5294a9a70f80cedec271
Semih Celek <cybermoon1999@gmail.com>
2021-04-06 16:57:41 +0300

    Clean up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 import React from "react";
-import StoriesPage from "./pages/Stories-Page";
-import Login from "./components/Login";
 import Router from "./components/Router";
 
 const App = () => {
